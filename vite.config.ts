ae4b40893705e01b40dfb967346b77ed5858bbd3
John Hazuka <126054523+JohnHazukaJr@users.noreply.github.com>
2026-08-03 21:11:07 -0700

    logo

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon.svg'],
+      includeAssets: ['favicon-32.png', 'icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'Small Worlds AI',
         short_name: 'Small Worlds',
